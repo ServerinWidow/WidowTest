9d334c695f42665851c56a6f6ef9a26c26152c2e
Federico Barra <federico.n.barra@gmail.com>
2015-12-30 14:40:32 -0300

    +

---
+++ ProjectTesting/Assets/Editor/TestingEditor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestingEditor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
