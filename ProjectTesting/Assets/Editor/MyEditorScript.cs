7eebf66d3f3267d24fe2125f71e7f61d63b331fd
Federico Barra <federico.n.barra@gmail.com>
2016-01-05 16:57:14 -0300

    asd

--- ProjectTesting/Assets/Editor/MyEditorScript.cs
+++ ProjectTesting/Assets/Editor/MyEditorScript.cs
@@ -11,7 +11,7 @@ public class MyEditorScript : MonoBehaviour
 
 	static string APP_NAME = "Testeo";
 	static string TARGET_DIR = "C:/Main/Builds/Builds Windows/Testeo Widow Build";
-	static string TARGET_DIR_ANDROID = "C:/Main/Builds/Builds Windows/";
+	static string TARGET_DIR_ANDROID = "C:/Main/Repositories/TesteoRepo";
 
 	[MenuItem ("Custom/CI/Build Widows")]
 	static void PerformWindowsBuild ()
