108a263301096e216e5bb170d5ba068c4d16a282
Federico Barra <federico.n.barra@gmail.com>
2016-01-06 11:56:16 -0300

    +jenkins build script add-on

--- ProjectTesting/Assets/Editor/MyEditorScript.cs
+++ ProjectTesting/Assets/Editor/MyEditorScript.cs
@@ -1,49 +1,107 @@
 ﻿using UnityEngine;
-using System.Collections;
 using UnityEditor;
 using System.Collections.Generic;
 using System;
+using UnityEditor.UI;
 
-public class MyEditorScript : MonoBehaviour 
+public class MyEditorScript : MonoBehaviour
 {
+    private static bool DebugOn;
+    static string[] Scenes = FindEnabledEditorScenes();
 
-	static string[] SCENES = FindEnabledEditorScenes();
+    static string GameName = "Testeo";
+    static string TargetDir = "C:/";
+    private static string platform = "android";
 
-	static string APP_NAME = "Testeo";
-	static string TARGET_DIR = "C:/Main/Builds/Builds Windows/Testeo Widow Build";
-	static string TARGET_DIR_ANDROID = "C:/Main/Repositories/TesteoRepo";
+    private static Dictionary<string, BuildTarget> BuildPlatforms = new Dictionary<string, BuildTarget>();
+    private static Dictionary<string, string> Extensions = new Dictionary<string, string>();
+    private static Dictionary<string, string> Arguments = new Dictionary<string, string>();
 
-	[MenuItem ("Custom/CI/Build Widows")]
-	static void PerformWindowsBuild ()
-	{
-		 string target_dir = APP_NAME + ".exe";
-		 GenericBuild(SCENES, TARGET_DIR + "/" + target_dir, BuildTarget.StandaloneWindows, BuildOptions.None);
-	}
+    static void InitData()
+    {
+        BuildPlatforms.Add("android", BuildTarget.Android);
+        BuildPlatforms.Add("pc", BuildTarget.StandaloneWindows);
+        BuildPlatforms.Add("ios", BuildTarget.iOS);
 
-	[MenuItem ("Custom/CI/Build Android")]
-	static void PerformAndroidBuild ()
-	{
-		 string target_dir = APP_NAME + ".apk";
-		 GenericBuild(SCENES, TARGET_DIR_ANDROID + "/" + target_dir, BuildTarget.Android, BuildOptions.None);
-	}
-	
-	private static string[] FindEnabledEditorScenes() {
-		List<string> EditorScenes = new List<string>();
-		foreach(EditorBuildSettingsScene scene in EditorBuildSettings.scenes) 
-		{
-			if (!scene.enabled) continue;
-				EditorScenes.Add(scene.path);
-		}
-		return EditorScenes.ToArray();
-	}
+        Extensions.Add("android", ".apk");
+        Extensions.Add("pc", ".exe");
+        Extensions.Add("ios", ".xcode");
+    }
 
-	static void GenericBuild(string[] scenes, string target_dir, BuildTarget build_target, BuildOptions build_options)
-	{
-		EditorUserBuildSettings.SwitchActiveBuildTarget(build_target);
-		string res = BuildPipeline.BuildPlayer(scenes,target_dir, build_target, build_options);
-		if (res.Length > 0) 
-		{
-			throw new Exception("BuildPlayer failure NOOOO: " + res);
-		}
-	}
+    [MenuItem(("Custom/CI/Run"))]
+    static void Run()
+    {
+        InitData();
+
+        GetCmdLineArguments();
+        SetBuildSpecifics();
+
+        string target_dir = GameName + Extensions[platform];
+        DebugLog("target_dir: " + target_dir);
+        DebugLog("BuildPlatforms[platform]: " + BuildPlatforms[platform]);
+
+
+        GenericBuild(Scenes, TargetDir + "/" + target_dir, BuildPlatforms[platform], BuildOptions.None);
+    }
+
+    static void GetCmdLineArguments()
+    {
+        // unity -quit MyEditorScript.Run   ...    -args gameType:MEMORAMA,IOS:true
+        string cmdLine = System.Environment.CommandLine;
+
+        DebugOn = cmdLine.Contains("-debug");
+
+        string[] separatingChars = { "-args", "..." };
+
+        if (cmdLine.Contains("-args "))
+        {
+            Arguments = new Dictionary<string, string>();
+
+            cmdLine = cmdLine.Split(separatingChars, StringSplitOptions.None)[1];
+            string[] tuplas = cmdLine.Split(',');
+
+            foreach (string tupla in tuplas)
+            {
+                Arguments.Add(tupla.Split(':')[0].TrimEnd().TrimStart().ToLower(), tupla.Split(':')[1].TrimEnd().TrimStart().ToLower());
+            }
+        }
+
+        DebugLog("Arguments:");
+        foreach (var argument in Arguments)
+        {
+            DebugLog("[" + argument.Key + " = " + argument.Value + "]");
+        }
+    }
+
+    static void SetBuildSpecifics()
+    {
+        platform = Arguments.ContainsKey("platform") ? Arguments["platform"] : platform;
+
+    }
+
+    private static string[] FindEnabledEditorScenes()
+    {
+        List<string> EditorScenes = new List<string>();
+        foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
+        {
+            if (!scene.enabled) continue;
+            EditorScenes.Add(scene.path);
+        }
+        return EditorScenes.ToArray();
+    }
+
+    static void GenericBuild(string[] scenes, string target_dir, BuildTarget build_target, BuildOptions build_options)
+    {
+        EditorUserBuildSettings.SwitchActiveBuildTarget(build_target);
+        string res = BuildPipeline.BuildPlayer(scenes, target_dir, build_target, build_options);
+        if (res.Length > 0)
+        {
+            throw new Exception("BuildPlayer failure NOOOO: " + res);
+        }
+    }
+    static void DebugLog(object p)
+    {
+        if (DebugOn)
+            Debug.Log(p + "\n");
+    }
 }
