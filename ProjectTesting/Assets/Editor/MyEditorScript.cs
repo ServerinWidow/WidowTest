1042219701ca224d68503a81621a1001feba05d7
Jenkins <jenkins@Administradors-Mac-mini.local>
2016-01-25 18:00:40 -0300

    Fixed Script Error.

--- ProjectTesting/Assets/Editor/MyEditorScript.cs
+++ ProjectTesting/Assets/Editor/MyEditorScript.cs
@@ -1,131 +1,131 @@
 ﻿using UnityEngine;
 using UnityEditor;
+using UnityEngine;
+using UnityEditor;
 using System.Collections.Generic;
 using System;
 using UnityEditor.UI;
 
 public class MyEditorScript : MonoBehaviour
 {
-    private static bool DebugOn;
-    private static string[] Scenes = FindEnabledEditorScenes();
-
-    private static string GameName = "Testeo";
+	private static bool DebugOn;
+	private static string[] Scenes = FindEnabledEditorScenes();
+	
+	private static string GameName = "Testeo";
 	private static string ProjectWorkspace = "/Users/Shared/Jenkins/Home/jobs/";
-
-    private static string platform = "android";
-
-    private static Dictionary<string, BuildTarget> BuildPlatforms = new Dictionary<string, BuildTarget>();
-    private static Dictionary<string, string> Extensions = new Dictionary<string, string>();
-    private static Dictionary<string, string> Arguments = new Dictionary<string, string>();
-
-    static void InitData()
-    {
-        BuildPlatforms.Add("android", BuildTarget.Android);
-        BuildPlatforms.Add("pc", BuildTarget.StandaloneWindows);
+	
+	private static string platform = "android";
+	
+	private static Dictionary<string, BuildTarget> BuildPlatforms = new Dictionary<string, BuildTarget>();
+	private static Dictionary<string, string> Extensions = new Dictionary<string, string>();
+	private static Dictionary<string, string> Arguments = new Dictionary<string, string>();
+	
+	static void InitData()
+	{
+		BuildPlatforms.Add("android", BuildTarget.Android);
+		BuildPlatforms.Add("pc", BuildTarget.StandaloneWindows);
 		BuildPlatforms.Add("osx", BuildTarget.StandaloneOSXIntel);
-        BuildPlatforms.Add("ios", BuildTarget.iOS);
-
-        Extensions.Add("android", ".apk");
-        Extensions.Add("pc", ".exe");
+		BuildPlatforms.Add("ios", BuildTarget.iOS);
+		
+		Extensions.Add("android", ".apk");
+		Extensions.Add("pc", ".exe");
 		Extensions.Add("osx", ".app");
-        Extensions.Add("ios", ".xcode");
-    }
-
-    [MenuItem(("Custom/CI/Run"))]
-    static void Run()
-    {
-        InitData();
-
-        GetCmdLineArguments();
-        SetBuildSpecifics();
-
-        string buildName = GameName + Extensions[platform];
-        DebugLog("target_dir: " + buildName);
-        DebugLog("BuildPlatforms[platform]: " + BuildPlatforms[platform]);
-        DebugLog("workspace: " + ProjectWorkspace);
-
-
-        GenericBuild(Scenes, ProjectWorkspace + "/" + buildName, BuildPlatforms[platform], BuildOptions.None);
-    }
-
-    static void GetCmdLineArguments()
-    {
-        // unity -quit MyEditorScript.Run   ...    -args gameType=MEMORAMA,IOS=true
-        string cmdLine = System.Environment.CommandLine;
-
-        DebugOn = cmdLine.Contains("-debug");
-
-        string[] separatingChars = { "-args", "..." };
-
-        if (cmdLine.Contains("-args "))
-        {
-            Arguments = new Dictionary<string, string>();
-
-            cmdLine = cmdLine.Split(separatingChars, StringSplitOptions.None)[1];
-            string[] tuplas = cmdLine.Split(',');
-
-            foreach (string tupla in tuplas)
-            {
-                Arguments.Add(tupla.Split('=')[0].TrimEnd().TrimStart(), tupla.Split('=')[1].TrimEnd().TrimStart());
-            }
-        }
-
-        DebugLog("Arguments:");
-        foreach (var argument in Arguments)
-        {
-            DebugLog("[" + argument.Key + " = " + argument.Value + "]");
-        }
-    }
-
-    static void SetBuildSpecifics()
-    {
-        platform = Arguments.ContainsKey("PLATFORM") ? Arguments["PLATFORM"] : platform;
-        ProjectWorkspace = Arguments.ContainsKey("WORKSPACE") ? Arguments["WORKSPACE"] : ProjectWorkspace;
-        GameName = Arguments.ContainsKey("GAME") ? Arguments["GAME"] : GameName;
-        
-
+		Extensions.Add("ios", ".xcode");
+	}
+	
+	[MenuItem(("Custom/CI/Run"))]
+	static void Run()
+	{
+		InitData();
+		
+		GetCmdLineArguments();
+		SetBuildSpecifics();
+		
+		string buildName = GameName + Extensions[platform];
+		DebugLog("target_dir: " + buildName);
+		DebugLog("BuildPlatforms[platform]: " + BuildPlatforms[platform]);
+		DebugLog("workspace: " + ProjectWorkspace);
+		
+		
+		GenericBuild(Scenes, ProjectWorkspace + "/" + buildName, BuildPlatforms[platform], BuildOptions.None);
+	}
+	
+	static void GetCmdLineArguments()
+	{
+		// unity -quit MyEditorScript.Run   ...    -args gameType=MEMORAMA,IOS=true
+		string cmdLine = System.Environment.CommandLine;
+		
+		DebugOn = cmdLine.Contains("-debug");
+		
+		string[] separatingChars = { "-args", "..." };
+		
+		if (cmdLine.Contains("-args "))
+		{
+			Arguments = new Dictionary<string, string>();
+			
+			cmdLine = cmdLine.Split(separatingChars, StringSplitOptions.None)[1];
+			string[] tuplas = cmdLine.Split(',');
+			
+			foreach (string tupla in tuplas)
+			{
+				Arguments.Add(tupla.Split('=')[0].TrimEnd().TrimStart(), tupla.Split('=')[1].TrimEnd().TrimStart());
+			}
+		}
+		
+		DebugLog("Arguments:");
+		foreach (var argument in Arguments)
+		{
+			DebugLog("[" + argument.Key + " = " + argument.Value + "]");
+		}
+	}
+	
+	static void SetBuildSpecifics()
+	{
+		platform = Arguments.ContainsKey("PLATFORM") ? Arguments["PLATFORM"] : platform;
+		ProjectWorkspace = Arguments.ContainsKey("WORKSPACE") ? Arguments["WORKSPACE"] : ProjectWorkspace;
+		GameName = Arguments.ContainsKey("GAME") ? Arguments["GAME"] : GameName;
+		
+		
 		if (Arguments.ContainsKey("DEFINES"))
-        {
-            string[] defines = Arguments["DEFINES"].Split(';');
-            DebugLog("DEFINES: " + defines.ToString());
-            foreach (string define in defines)
-            {
-
-                PlayerSettings.SetScriptingDefineSymbolsForGroup(BuildTargetGroup.Android, define);
-                PlayerSettings.SetScriptingDefineSymbolsForGroup(BuildTargetGroup.StandaloneWindows, define);
-				PlayerSettings.SetScriptingDefineSymbolsForGroup(BuildTargetGroup.StandaloneOSXIntel, define);
-                PlayerSettings.SetScriptingDefineSymbolsForGroup(BuildTargetGroup.iOS, define);
-                DebugLog(define + ", ");
-            }
-
-
-        }
-
-    }
-
-    private static string[] FindEnabledEditorScenes()
-    {
-        List<string> EditorScenes = new List<string>();
-        foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
-        {
-            if (!scene.enabled) continue;
-            EditorScenes.Add(scene.path);
-        }
-        return EditorScenes.ToArray();
-    }
-
-    static void GenericBuild(string[] scenes, string target_dir, BuildTarget build_target, BuildOptions build_options)
-    {
-        EditorUserBuildSettings.SwitchActiveBuildTarget(build_target);
-        string res = BuildPipeline.BuildPlayer(scenes, target_dir, build_target, build_options);
-        if (res.Length > 0)
-        {
-            throw new Exception("BuildPlayer failure NOOOO: " + res);
-        }
-    }
-    static void DebugLog(object p)
-    {
-        if (DebugOn)
-            Debug.Log("XXXXXXXXXXX " + p + "\n");
-    }
+		{
+			string[] defines = Arguments["DEFINES"].Split(';');
+			DebugLog("DEFINES: " + defines.ToString());
+			foreach (string define in defines)
+			{
+				PlayerSettings.SetScriptingDefineSymbolsForGroup(BuildTargetGroup.Android, define);
+				PlayerSettings.SetScriptingDefineSymbolsForGroup(BuildTargetGroup.Standalone, define);
+				PlayerSettings.SetScriptingDefineSymbolsForGroup(BuildTargetGroup.iOS, define);
+				DebugLog(define + ", ");
+			}
+			
+			
+		}
+		
+	}
+	
+	private static string[] FindEnabledEditorScenes()
+	{
+		List<string> EditorScenes = new List<string>();
+		foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
+		{
+			if (!scene.enabled) continue;
+			EditorScenes.Add(scene.path);
+		}
+		return EditorScenes.ToArray();
+	}
+	
+	static void GenericBuild(string[] scenes, string target_dir, BuildTarget build_target, BuildOptions build_options)
+	{
+		EditorUserBuildSettings.SwitchActiveBuildTarget(build_target);
+		string res = BuildPipeline.BuildPlayer(scenes, target_dir, build_target, build_options);
+		if (res.Length > 0)
+		{
+			throw new Exception("BuildPlayer failure NOOOO: " + res);
+		}
+	}
+	static void DebugLog(object p)
+	{
+		if (DebugOn)
+			Debug.Log("XXXXXXXXXXX " + p + "\n");
+	}
 }
