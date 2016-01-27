e7b5189f85d914e87ff9c059db65ca03684ff29c
Jenkins <jenkins@Administradors-Mac-mini.local>
2016-01-27 12:41:01 -0300

    Change for testing Jenkins.

--- ProjectTesting/Assets/Editor/MyEditorScript.cs
+++ ProjectTesting/Assets/Editor/MyEditorScript.cs
@@ -42,10 +42,10 @@ public class MyEditorScript : MonoBehaviour
 		
 		string buildName;
 		try { buildName = GameName + Extensions[platform]; }
-		catch (Exception e) { buildName = "XCodeProject"; }
+		catch (Exception e) { buildName = "/XCodeProject"; }
 		DebugLog("target_dir: " + buildName);
 
-		GenericBuild(Scenes, ProjectWorkspace, BuildPlatforms[platform], BuildOptions.None);
+		GenericBuild(Scenes, ProjectWorkspace + "/" + buildName, BuildPlatforms[platform], BuildOptions.None);
 	}
 	
 	static void GetCmdLineArguments()
